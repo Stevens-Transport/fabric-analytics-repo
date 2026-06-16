903110964ffbfbb39ba6ee35632e21853feb7e55
ravipenmetsastevens <Raju.Penmesta@stevenstransport.com>
2026-06-16 15:08:12 -0500

    Committing 23 items from workspace a6d2c31b-0b03-4c60-a258-c2664c56fe3d

---
+++ StagingWarehouseForDataflows_20250804144627.Warehouse/dbo/Tables/4df17b9270144967a2154f6a10765a43_e9d33bf6_002D5202_002D4418_002Dbb83_002D9613149af429.sql
@@ -0,0 +1,17 @@
+CREATE TABLE [dbo].[4df17b9270144967a2154f6a10765a43_e9d33bf6_002D5202_002D4418_002Dbb83_002D9613149af429] (
+
+	[Column1] varchar(8000) NULL, 
+	[Column2] varchar(8000) NULL, 
+	[Column3] varchar(8000) NULL, 
+	[Column4] varchar(8000) NULL, 
+	[Column5] varchar(8000) NULL, 
+	[Column6] bigint NULL, 
+	[Column7] varchar(8000) NULL, 
+	[Column8] varchar(8000) NULL, 
+	[Column9] varchar(8000) NULL, 
+	[Column10] bigint NULL, 
+	[Column11] bigint NULL, 
+	[Column12] date NULL, 
+	[Column13] varchar(8000) NULL, 
+	[Column14] varchar(8000) NULL
+);
